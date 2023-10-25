596c2a14b119bf514bd8d1defa752b43c66459cd
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-10-25 08:51:59 +0200

    Create 04_insert_data_into_employees_table.sql

---
+++ 03_basic_CRUD/04_insert_data_into_employees_table.sql
@@ -0,0 +1,8 @@
+-- Insert new records
+INSERT INTO employees (first_name, last_name, job_title, department_id, salary)
+VALUES
+    ('Samantha', 'Young', 'Housekeeping', 4, 900),
+    ('Roger', 'Palmer', 'Waiter', 3, 928.33);
+
+-- Select all employees' info
+SELECT * FROM employees;
