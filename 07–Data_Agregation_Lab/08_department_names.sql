efc1562083bed3c727b9f85a50c7634352082545
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-02 10:08:30 +0100

    Create 08_department_names.sql

---
+++ 07–Data_Agregation_Lab/08_department_names.sql
@@ -0,0 +1,14 @@
+SELECT
+	id,
+	first_name,
+	last_name,
+	trunc(salary, 2),
+	department_id,
+	CASE department_id
+		WHEN 1 THEN 'Management'
+		WHEN 2 THEN 'Kitchen Staff'
+		WHEN 3 THEN 'Service Staff'
+		ELSE 'Other'
+	END AS department_name
+FROM
+	employees;
