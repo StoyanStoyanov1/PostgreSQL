2d4d0fa8b2c539cad5b8f06d1f49610920f4c36f
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-07 09:18:03 +0100

    Create 15_countries_without_any_rivers.sql

---
+++ 10_Table_Relations_Exercise/15_countries_without_any_rivers.sql
@@ -0,0 +1,8 @@
+SELECT
+	count(*) AS countries_without_rivers
+FROM countries
+LEFT JOIN countries_rivers
+	ON countries_rivers.country_code = countries.country_code
+-- 	USING (country_code)
+WHERE
+	countries_rivers.country_code IS NULL;
