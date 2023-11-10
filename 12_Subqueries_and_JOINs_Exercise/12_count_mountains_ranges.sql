bb51e236842a8cb73346fd081b02dba290f4d2c8
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-10 08:21:19 +0100

    Create 12_count_mountains_ranges.sql

---
+++ 12_Subqueries_and_JOINs_Exercise/12_count_mountains_ranges.sql
@@ -0,0 +1,8 @@
+SELECT
+	mc.country_code,
+	count(m.mountain_range) AS mountain_range_count
+FROM mountains AS m
+JOIN mountains_countries AS mc ON mc.mountain_id = m.id
+WHERE mc.country_code IN ('US', 'RU', 'BG')
+GROUP BY mc.country_code
+ORDER BY mountain_range_count DESC;
