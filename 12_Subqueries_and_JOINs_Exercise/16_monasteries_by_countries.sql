37d64f7201a5bcd69336320eae3423dfebb54e51
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-11 18:20:01 +0100

    Create 16_monasteries_by_countries.sql

---
+++ 12_Subqueries_and_JOINs_Exercise/16_monasteries_by_countries.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS monasteries (
+  "id" SERIAL PRIMARY KEY,
+  monastery_name VARCHAR(255),
+  country_code CHAR(2)
+);
+
+INSERT INTO monasteries (monastery_name, country_code)
+VALUES
+	('Rila Monastery "St. Ivan of Rila"', 'BG'),
+	('Bachkovo Monastery "Virgin Mary"', 'BG'),
+	('Troyan Monastery "Holy Mother''s Assumption"', 'BG'),
+	('Kopan Monastery', 'NP'),
+	('Thrangu Tashi Yangtse Monastery', 'NP'),
+	('Shechen Tennyi Dargyeling Monastery', 'NP'),
+	('Benchen Monastery', 'NP'),
+	('Southern Shaolin Monastery', 'CN'),
+	('Dabei Monastery', 'CN'),
+	('Wa Sau Toi', 'CN'),
+	('Lhunshigyia Monastery', 'CN'),
+	('Rakya Monastery', 'CN'),
+	('Monasteries of Meteora', 'GR'),
+	('The Holy Monastery of Stavronikita', 'GR'),
+	('Taung Kalat Monastery', 'MM'),
+	('Pa-Auk Forest Monastery', 'MM'),
+	('Taktsang Palphug Monastery', 'BT'),
+	('Sümela Monastery', 'TR');
+
+ALTER TABLE countries
+ADD COLUMN three_rivers BOOLEAN DEFAULT FALSE;
+
+UPDATE countries
+SET three_rivers = TRUE
+WHERE country_code IN (
+	SELECT country_code
+	FROM countries_rivers
+	GROUP BY country_code
+	HAVING count(*) > 3
+);
+
+SELECT m.monastery_name, c.country_name
+FROM monasteries m
+JOIN countries c USING (country_code)
+WHERE NOT c.three_rivers
+ORDER BY m.monastery_name ASC;
