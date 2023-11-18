e6fa5f7d1af0a77c13f3bad950ae1bbb7eee7686
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-18 23:47:35 +0100

    Create 3.6_animals_data.sql

---
+++ Exam_Preparation_I/3.6_animals_data.sql
@@ -0,0 +1,12 @@
+SELECT
+    a."name",
+    at.animal_type,
+    TO_CHAR(a.birthdate, 'DD.MM.YYYY') AS birthdate
+FROM
+    animals AS a
+JOIN
+    animal_types AS at
+ON
+    a.animal_type_id = at.id
+ORDER BY
+    a.name ASC;
