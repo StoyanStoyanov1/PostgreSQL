6a2ea6089ab425d9507b7dc35bcbe7e9df9eab27
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-19 08:19:09 +0100

    Create 2.2_insert.sql

---
+++ Exam_Preparation_II/2.2_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO clients (
+	full_name, phone_number
+)
+SELECT
+    CONCAT(d.first_name, ' ', d.last_name) AS full_name,
+    '(088) 9999' || (d.id * 2) AS phone_number
+FROM drivers AS d
+WHERE d.id BETWEEN 10 AND 20;
