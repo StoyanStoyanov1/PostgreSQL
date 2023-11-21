f5c120d7f8a409d4d6716a66ffa080b712cde7c1
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-21 08:01:33 +0100

    Create 3.7_number_of_courses_for_each_car.sql

---
+++ Exam_Preparation_II/3.7_number_of_courses_for_each_car.sql
@@ -0,0 +1,14 @@
+SELECT
+    c.id AS car_id,
+    c.make,
+    c.mileage,
+    COUNT(co.id) AS count_of_courses,
+    ROUND(AVG(co.bill), 2) AS average_bill
+FROM cars c
+LEFT JOIN courses co ON c.id = co.car_id
+GROUP BY
+    c.id, c.make, c.model
+HAVING
+    COUNT(co.id) <> 2
+ORDER BY
+    count_of_courses DESC, car_id;
