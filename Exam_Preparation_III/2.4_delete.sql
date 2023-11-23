f85c3805eb08187667edc600e430934869b436a7
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2023-11-23 06:34:17 +0100

    Create 2.4_delete.sql

---
+++ Exam_Preparation_III/2.4_delete.sql
@@ -0,0 +1,20 @@
+DELETE FROM board_games
+WHERE publisher_id IN (
+    SELECT id
+    FROM publishers
+    WHERE address_id IN (
+        SELECT id
+        FROM addresses
+        WHERE town LIKE 'L%'
+    )
+);
+
+DELETE FROM publishers
+WHERE address_id IN (
+    SELECT id
+    FROM addresses
+    WHERE town LIKE 'L%'
+);
+
+DELETE FROM addresses
+WHERE town LIKE 'L%';
